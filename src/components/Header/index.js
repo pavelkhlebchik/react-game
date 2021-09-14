4f78358fa1766d769d0b784c80bf1282e9197a96
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-09-14 22:51:59 +0300

    feature: add routes Game and Home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
 import header from './header.module.css';
 
-const Header = ({title, descr}) => {
+const Header = ({title, descr, onClickButton}) => {
+  const handlerClick = () => {
+    console.log('#### <Header />');
+    onClickButton && onClickButton('game');
+  }
   return (
     <header className={header.root}>
       <div className={header.forest}></div>
       <div className={header.container}>
         {title && (<h1>{title}</h1>)}
         {descr && (<p>{descr}</p>)}
+        <button onClick={handlerClick}>
+          Start Game
+        </button>
       </div>
     </header>
   )
